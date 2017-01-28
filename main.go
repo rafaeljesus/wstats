4d84d16403b614a69dbb0dfb9e956eef80e49810
Rafael Jesus <rafaelljesus86@gmail.com>
2017-01-28 12:28:55 -0200

    Expose http healthz endpoint on main

--- main.go
+++ main.go
@@ -6,20 +6,28 @@ import (
 	"github.com/rafaeljesus/wstats/handlers"
 	"github.com/rafaeljesus/wstats/net"
 	"github.com/rafaeljesus/wstats/store"
+	"net/http"
 	"runtime"
 )
 
 func main() {
 	tcpPort := flag.String("tcp_port", "5555", "Set tcp port")
+	httpPort := flag.String("http_port", "8080", "Set http port")
 	flag.Parse()
 
 	numcpu := runtime.NumCPU()
 	runtime.GOMAXPROCS(numcpu)
 
 	store := store.NewStore()
-	env := handlers.NewEnv(store)
+	mux := http.NewServeMux()
+
+	env := handlers.NewEnv(store, mux)
 
 	log.WithField("tcp_port", tcpPort).Info("starting tcp server")
+	go net.ListenAndServeTCP(":"+*tcpPort, env.ReceiveChannel)
 
-	net.ListenAndServeTCP(":"+*tcpPort, env.ReceiveChannel)
+	mux.HandleFunc("/v1/healthz", env.Healthz)
+
+	log.WithField("http_port", httpPort).Info("starting http server")
+	http.ListenAndServe(":"+*httpPort, env)
 }
