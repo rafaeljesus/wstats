64ba9989075be1816352c3a213d7b9fc093e5fd1
Rafael Jesus <rafaelljesus86@gmail.com>
2017-01-29 14:45:37 -0200

    :nail_care: store tests

--- store/store_test.go
+++ store/store_test.go
@@ -77,20 +77,9 @@ func TestGetl(t *testing.T) {
 
 func TestSortByWords(t *testing.T) {
 	s := NewStore()
-	_, err := s.IncWmap("foo", 1)
-	if err != nil {
-		t.Errorf("Expected to increment word map %s", err)
-	}
-
-	_, err = s.IncWmap("foo", 1)
-	if err != nil {
-		t.Errorf("Expected to increment word map %s", err)
-	}
-
-	_, err = s.IncWmap("bar", 1)
-	if err != nil {
-		t.Errorf("Expected to increment word map %s", err)
-	}
+	s.IncWmap("foo", 1)
+	s.IncWmap("foo", 1)
+	s.IncWmap("bar", 1)
 
 	sorted, count := s.SortByWords()
 
@@ -105,32 +94,24 @@ func TestSortByWords(t *testing.T) {
 
 func TestSortByLetters(t *testing.T) {
 	s := NewStore()
-	_, err := s.IncLmap("foo", 1)
-	if err != nil {
-		t.Errorf("Expected to increment letter map %s", err)
-	}
-
-	_, err = s.IncLmap("foo", 1)
-	if err != nil {
-		t.Errorf("Expected to increment letter map %s", err)
-	}
-
-	_, err = s.IncLmap("bar", 1)
-	if err != nil {
-		t.Errorf("Expected to increment letter map %s", err)
-	}
+	s.IncLmap("a", 1)
+	s.IncLmap("a", 1)
+	s.IncLmap("a", 1)
+	s.IncLmap("b", 1)
+	s.IncLmap("b", 1)
+	s.IncLmap("c", 1)
 
 	sorted := s.SortByLetters()
 
-	if sorted[0] != "foo" && sorted[1] != "bar" {
+	if sorted[0] != "a" && sorted[1] != "b" && sorted[2] != "c" {
 		t.Errorf("Expected sort to ordered by top letters %s", sorted)
 	}
 }
 
 func TestCount(t *testing.T) {
 	s := NewStore()
-	_, _ = s.IncWmap("key", 1)
-	_, _ = s.IncLmap("key", 1)
+	s.IncWmap("key", 1)
+	s.IncLmap("key", 1)
 	if s.Count() != 2 {
 		t.Errorf("Expected 2 equal total count %s", strconv.Itoa(s.Count()))
 	}
